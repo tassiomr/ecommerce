84f9a410d06e2fdb09f045fa157495b57f05df86
Tassio Marcos Rocha <tassiomarcosr@outlook.com>
2022-02-18 11:57:02 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import {Body, Text} from '../../components';
+import {List} from '../../components/flatlist';
 
 export const Home: React.FC = () => {
+  const list = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
-    <Body isLoading={false} title='Home' showBackButton={false}>
-      <Text text={'Hello World'} />
+    <Body isLoading={false} title="Home" showBackButton={false}>
+      <List items={list} />
     </Body>
   );
 };
