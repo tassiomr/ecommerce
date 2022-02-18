b898c8949a2a350f2f983d77f6875e6885db6e8b
Tassio Marcos Rocha <tassiomarcosr@outlook.com>
2022-02-18 10:43:04 -0300

    Update index.tsx

--- src/components/body/index.tsx
+++ src/components/body/index.tsx
@@ -1,16 +1,27 @@
 import * as React from 'react';
 import {ActivityIndicator} from 'react-native';
+import {Nav} from '../nav';
 import {Component} from './styles';
 
 type Props = {
   children: React.ReactElement;
   isLoading?: boolean;
+  showBackButton?: boolean;
+  title: string;
 };
 
-export const Body: React.FC<Props> = ({children, isLoading}) => {
+export const Body: React.FC<Props> = ({
+  children,
+  isLoading,
+  showBackButton,
+  title,
+}) => {
   return (
-    <Component>
-      {isLoading ? <ActivityIndicator size="large" /> : children}
-    </Component>
+    <>
+      <Nav showBackButton={showBackButton} title={title} />
+      <Component>
+        {isLoading ? <ActivityIndicator size="large" /> : children}
+      </Component>
+    </>
   );
 };
