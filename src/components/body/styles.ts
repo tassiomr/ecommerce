8f6f8368f011a474762d1661ff44e9fc1e7a4009
Tassio Marcos Rocha <tassiomarcosr@outlook.com>
2022-02-18 11:58:29 -0300

    Update styles.ts

--- src/components/body/styles.ts
+++ src/components/body/styles.ts
@@ -1,12 +1,10 @@
+import {Dimensions} from 'react-native';
 import styled from 'styled-components/native';
 import {IStyledComponentsProps} from '../../typescript/interface';
 
 export const Component = styled.View`
   flex: 1;
-  width: 100%;
   alignitems: center;
-  padding-left: 8px;
-  padding-right: 8px;
   background-color: ${(props: IStyledComponentsProps) =>
     props.theme.colors.background};
 `;
