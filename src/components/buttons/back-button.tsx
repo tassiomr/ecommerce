294b913f0c36a27b32cff632b61f2860ac5a0c53
Tassio Marcos Rocha <tassiomarcosr@outlook.com>
2022-02-18 11:58:18 -0300

    Update back-button.tsx

--- src/components/buttons/back-button.tsx
+++ src/components/buttons/back-button.tsx
@@ -7,7 +7,7 @@ export const BackButton: React.FC = () => {
   const navigation = useNavigation();
 
   const pop = () => navigation.pop();
-  
+
   return (
     <Container onPress={navigation}>
       <Icon name="arrow-left" size="small" />
