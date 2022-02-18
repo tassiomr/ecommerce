e90f7d4382256df8dfcb4c65eb853cd4c1bad5bd
Tassio Marcos Rocha <tassiomarcosr@outlook.com>
2022-02-18 11:57:12 -0300

    Update styles.ts

--- src/components/text/styles.ts
+++ src/components/text/styles.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components/native';
 import {IStyledComponentsProps} from '../../typescript/interface';
+import {TextSize} from '../../typescript/type';
 
-export const Component = styled.Text`
-  color: ${(props: IStyledComponentsProps) => props.theme.colors.accent};
-  font-size: ${(props: IStyledComponentsProps) =>
-    props.theme.sizes.font.paragraph};
+export const Component = styled.Text<
+  IStyledComponentsProps & {textSize: TextSize}
+>`
+  color: ${({theme}) => theme.colors.accent};
+  font-size: ${({theme, textSize}) => theme.sizes.font[textSize]};
   font-weight: 700;
 `;
