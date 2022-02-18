e33ac6e03d938cba42c9fcc74a5a33828c8b17c8
Tassio Marcos Rocha <tassiomarcosr@outlook.com>
2022-02-18 11:57:15 -0300

    Update index.tsx

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {TextSize} from '../../typescript/type';
 import {Component} from './styles';
 
 type Props = {
   text: string;
+  textSize?: TextSize;
 };
 
-export const Text: React.FC<Props> = ({text}) => {
-  return <Component>{text}</Component>;
+export const Text: React.FC<Props> = ({text, textSize = TextSize.header}) => {
+  return <Component textSize={textSize}>{text}</Component>;
 };
