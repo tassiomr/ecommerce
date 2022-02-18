bb56064f241e7da7d6e2c2d568bf8de447605fd9
Tassio Marcos Rocha <tassiomarcosr@outlook.com>
2022-02-18 10:48:22 -0300

    Update index.tsx

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -3,7 +3,7 @@ import {Body, Text} from '../../components';
 
 export const Details: React.FC = () => {
   return (
-    <Body isLoading={false}>
+    <Body isLoading={false} title='Details'>
       <Text text={'Hello World'} />
     </Body>
   );
